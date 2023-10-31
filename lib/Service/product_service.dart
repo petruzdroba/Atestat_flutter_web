82f1999d28200d099c95f02b6a0363e8df901e33
Zdroba Petru <petru.zdroba@gmail.com>
2023-10-31 18:17:21 +0200

    (optional), get_product detailes service method in post with id sent in link with ?id=$id at the end in service

--- lib/Service/product_service.dart
+++ lib/Service/product_service.dart
@@ -13,9 +13,9 @@ Future<http.Response> getProductList() {
   );
 }
 
-Future<http.Response> getProductDetails() {
+Future<http.Response> getProductDetails(int id) {
   return http.get(
-    Uri.parse('https://cb5fbe06-e5ef-4282-b5e6-04b07cc24db8.mock.pstmn.io/product_details'),
+    Uri.parse('https://cb5fbe06-e5ef-4282-b5e6-04b07cc24db8.mock.pstmn.io/product_details?id=$id'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
